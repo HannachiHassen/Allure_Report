7aa54c25e82be9d62cf10aca17b26b0164e0dacd
Hassen Hannachi <hannachihassen07@gmail.com>
2022-09-25 17:42:51 -0400

    Allure report first commit

---
+++ src/main/java/com/utils/ExtentTestManager.java
@@ -0,0 +1,33 @@
+package com.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public final class ExtentTestManager {
+
+	/**
+	 * extentTestMap holds the information of thread ids and ExtentTest instances.
+	 * ExtentReports instance created by calling getExtentReports() method from
+	 * ExtentManager. At startTest() method, an instance of ExtentTest created and
+	 * put into extentTestMap with current thread id. At getTest() method, return
+	 * ExtentTest instance in extentTestMap by using current thread id.
+	 */
+	public ExtentTestManager() {}
+
+	static Map<Integer, ExtentTest> extentTestMap = new HashMap<>();
+	static ExtentReports extent = ExtentManager.getExtentReports();
+
+	public static synchronized ExtentTest getTest() {
+		return extentTestMap.get((int) Thread.currentThread().getId());
+	}
+
+	public static synchronized ExtentTest startTest(String testName, String desc) {
+		ExtentTest test = extent.createTest(testName, desc);
+		extentTestMap.put((int) Thread.currentThread().getId(), test);
+		return test;
+	}
+
+}
---
+++ src/main/java/logs/JSErrorLog.java
@@ -0,0 +1,19 @@
+package logs;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogType;
+
+public final class JSErrorLog {	
+
+	public JSErrorLog (){}
+	
+	    public static LogEntries getLogs(WebDriver driver) {
+	        return driver.manage().logs().get(LogType.BROWSER);
+	    }
+
+	    public static Boolean isLoginErrorLog(WebDriver driver) {
+	        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+	        return logEntries.getAll().stream()
+	            .anyMatch(logEntry -> logEntry.getMessage().contains("An invalid email address was specified"));
+	    }
+}
